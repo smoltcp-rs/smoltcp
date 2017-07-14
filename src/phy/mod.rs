c5fc8f7da702c47d71ef411aaaa55689869cc749
whitequark <whitequark@whitequark.org>
2017-07-14 03:30:00 +0000

    Document the loopback.

--- src/phy/mod.rs
+++ src/phy/mod.rs
@@ -1,12 +1,15 @@
 //! Access to networking hardware.
 //!
 //! The `phy` module deals with the *network devices*. It provides a trait
-//! for transmitting and receiving frames, [Device](trait.Device.html),
-//! as well as an implementations of that trait that uses the host OS,
-//! [RawSocket](struct.RawSocket.html) and [TapInterface](struct.TapInterface.html).
+//! for transmitting and receiving frames, [Device](trait.Device.html)
+//! and implementations of it:
 //!
-//! It also provides the _middleware interfaces_ [Tracer](struct.Tracer.html) and
-//! [FaultInjector](struct.FaultInjector.html), to facilitate debugging.
+//!   * the [_loopback_](struct.Loopback.html), for zero dependency testing;
+//!   * _middleware_ [Tracer](struct.Tracer.html) and
+//!     [FaultInjector](struct.FaultInjector.html), to facilitate debugging;
+//!   * _adapters_ [RawSocket](struct.RawSocket.html) and
+//!     [TapInterface](struct.TapInterface.html), to transmit and receive frames
+//!     on the host OS.
 //!
 // https://github.com/rust-lang/rust/issues/38740
 //! <h1 id="examples" class="section-header"><a href="#examples">Examples</a></h1>
