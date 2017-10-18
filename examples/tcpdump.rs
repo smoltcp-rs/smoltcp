7d5b2174bba47f3e67154fdf173058666903925c
Philipp Oppermann <dev@phil-opp.com>
2017-10-18 17:49:21 +0200

    Update tcpdump example

--- examples/tcpdump.rs
+++ examples/tcpdump.rs
@@ -1,14 +1,17 @@
 extern crate smoltcp;
 
 use std::env;
-use smoltcp::phy::{Device, RawSocket};
+use smoltcp::phy::{Device, RxToken, RawSocket};
 use smoltcp::wire::{PrettyPrinter, EthernetFrame};
 
 fn main() {
     let ifname = env::args().nth(1).unwrap();
     let mut socket = RawSocket::new(ifname.as_ref()).unwrap();
     loop {
-        let buffer = socket.receive(/*timestamp=*/0).unwrap();
-        print!("{}", PrettyPrinter::<EthernetFrame<&[u8]>>::new("", &buffer))
+        let (rx_token, _) = socket.receive().unwrap();
+        rx_token.consume(/*timestamp = */ 0, |buffer| {
+            print!("{}", PrettyPrinter::<EthernetFrame<&[u8]>>::new("", &buffer));
+            Ok(())
+        }).unwrap();
     }
 }
