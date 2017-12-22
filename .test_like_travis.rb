ecf6bf291f925d0a84d6824c70d852f1a8905fe1
whitequark <whitequark@whitequark.org>
2017-12-22 09:07:46 +0000

    Add a script to run every test Travis would run locally.

---
+++ .test_like_travis.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+require 'yaml'
+
+travis_config = YAML.load_file('.travis.yml')
+travis_config['matrix']['include'].each do |env|
+  ENV['RUSTUP_TOOLCHAIN'] = env['rust']
+  env['env'].scan(/(\w+)=\'(.+?)\'/) do
+    ENV[$1] = $2
+  end
+  travis_config['script'].each do |cmd|
+    $stderr.puts('+ #{cmd}'.gsub(/\$(\w+)/) { ENV[$1] })
+    system(cmd)
+  end
+end
