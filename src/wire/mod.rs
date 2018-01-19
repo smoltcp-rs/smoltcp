6a89a57ee4f1072ba3a0a6494d3341c529279de4
Herman J. Radtke III <herman@hermanradtke.com>
2018-01-19 08:25:06 -0800

    indentation fix

--- src/wire/mod.rs
+++ src/wire/mod.rs
@@ -127,8 +127,8 @@ pub use self::ipv6::{Address as Ipv6Address,
 
 #[cfg(feature = "proto-ipv6")]
 pub use self::ipv6option::{Packet as Ipv6OptionPacket,
-                        Repr as Ipv6OptionRepr,
-                        OptionType as Ipv6OptionType};
+                           Repr as Ipv6OptionRepr,
+                           OptionType as Ipv6OptionType};
 
 #[cfg(feature = "proto-ipv4")]
 pub use self::icmpv4::{Message as Icmpv4Message,
