8a868490deaf1b99f41c0cb514d57302d801343e
Kornel Lesiński <kornel@cloudflare.com>
2019-08-02 18:08:37 +0100

    Fix tests

--- src/wire/tcp.rs
+++ src/wire/tcp.rs
@@ -1187,16 +1187,16 @@ mod test {
                               &[0x03, 0x03, 0x0c]);
         assert_option_parses!(TcpOption::SackPermitted,
                               &[0x4, 0x02]);
-        assert_option_parses!(TcpOption::SackRange([Some((500, 1500)), None, None]),
+        assert_option_parses!(TcpOption::SackRange([Some((500, 1500)), None, None, None]),
                               &[0x05, 0x0a,
                                 0x00, 0x00, 0x01, 0xf4, 0x00, 0x00, 0x05, 0xdc]);
-        assert_option_parses!(TcpOption::SackRange([Some((875, 1225)), Some((1500, 2500)), None]),
+        assert_option_parses!(TcpOption::SackRange([Some((875, 1225)), Some((1500, 2500)), None, None]),
                               &[0x05, 0x12,
                                 0x00, 0x00, 0x03, 0x6b, 0x00, 0x00, 0x04, 0xc9,
                                 0x00, 0x00, 0x05, 0xdc, 0x00, 0x00, 0x09, 0xc4]);
         assert_option_parses!(TcpOption::SackRange([Some((875000, 1225000)),
                                                     Some((1500000, 2500000)),
-                                                    Some((876543210, 876654320))]),
+                                                    Some((876543210, 876654320)), None]),
                               &[0x05, 0x1a,
                                 0x00, 0x0d, 0x59, 0xf8, 0x00, 0x12, 0xb1, 0x28,
                                 0x00, 0x16, 0xe3, 0x60, 0x00, 0x26, 0x25, 0xa0,
