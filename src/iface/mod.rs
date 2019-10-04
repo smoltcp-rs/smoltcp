8feda112f853199fbeaf7e9caa488bfdfba54223
Ruben De Smet <ruben.de.smet@rubdos.be>
2019-10-04 11:10:20 +0200

    Add ethernet feature-gate as default.
    Disable ethernet-related features and framing based on a feature gate.
    
    Add a no-ethernet option to Travis.
    
    notes:
    - allow(unused) is added when not using ethernet feature
    - Don't run pretty-print doctest, ethernet is optional.

--- src/iface/mod.rs
+++ src/iface/mod.rs
@@ -4,13 +4,19 @@ The `iface` module deals with the *network interfaces*. It filters incoming fram
 provides lookup and caching of hardware addresses, and handles management packets.
 */
 
+#[cfg(feature = "ethernet")]
 mod neighbor;
 mod route;
+#[cfg(feature = "ethernet")]
 mod ethernet;
 
+#[cfg(feature = "ethernet")]
 pub use self::neighbor::Neighbor as Neighbor;
+#[cfg(feature = "ethernet")]
 pub(crate) use self::neighbor::Answer as NeighborAnswer;
+#[cfg(feature = "ethernet")]
 pub use self::neighbor::Cache as NeighborCache;
 pub use self::route::{Route, Routes};
+#[cfg(feature = "ethernet")]
 pub use self::ethernet::{Interface as EthernetInterface,
                          InterfaceBuilder as EthernetInterfaceBuilder};
