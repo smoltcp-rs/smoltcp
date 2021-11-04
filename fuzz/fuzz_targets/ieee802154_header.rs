d9f114a2aba781fdaf5fc3368b17c62c6671d854
Thibaut Vandervelden <thvdveld@vub.be>
2021-11-04 11:33:26 +0100

    Add fuzzing for IEEE802.15.4
    Because IEEE802.15.4 uses a lot of compression in its frame, fuzzing it
    is maybe a good idea. Adding this fuzz target showed that some frame
    methods were panicking. `check_len` now checks if accessors will panic
    or not.
    
    I ran the fuzzer for about 15 minutes and nothing showed up.

---
+++ fuzz/fuzz_targets/ieee802154_header.rs
@@ -0,0 +1,19 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use smoltcp::wire::{Ieee802154Frame, Ieee802154Repr};
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(ref frame) = Ieee802154Frame::new_checked(data) {
+        if let Ok(repr) = Ieee802154Repr::parse(frame) {
+            // The buffer len returns only the lenght required for emitting the header
+            // and does not take into account the length of the payload.
+            let mut buffer = vec![0; repr.buffer_len()];
+
+            // NOTE: unchecked because the checked version checks if the addressing mode field
+            // is valid or not. The addressing mode field is required for calculating the length of
+            // the header, which is used in `check_len`.
+            let mut frame = Ieee802154Frame::new_unchecked(&mut buffer[..]);
+            repr.emit(&mut frame);
+        }
+    };
+});
