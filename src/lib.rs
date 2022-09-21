092aba1a1c9492aa253c691c5f536f88220a37af
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2022-09-21 23:23:07 +0200

    Also rename socket-dhcpv4 in error message.

--- src/lib.rs
+++ src/lib.rs
@@ -108,7 +108,7 @@ compile_error!("You must enable at least one of the following features: proto-ip
         feature = "socket-dns",
     ))
 ))]
-compile_error!("If you enable the socket feature, you must enable at least one of the following features: socket-raw, socket-udp, socket-tcp, socket-icmp, socket-dhcp, socket-dns");
+compile_error!("If you enable the socket feature, you must enable at least one of the following features: socket-raw, socket-udp, socket-tcp, socket-icmp, socket-dhcpv4, socket-dns");
 
 #[cfg(all(
     feature = "socket",
