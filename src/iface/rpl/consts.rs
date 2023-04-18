6513c941de0b6b37c1d76d4b728444aae4ea81d6
Thibaut Vandervelden <thvdveld@vub.be>
2023-04-18 15:00:43 +0200

    Add RPL Rank logic
    Signed-off-by: Thibaut Vandervelden <thvdveld@vub.be>

--- src/iface/rpl/consts.rs
+++ src/iface/rpl/consts.rs
@@ -1,5 +1,7 @@
 pub const SEQUENCE_WINDOW: u8 = 16;
 
+pub const DEFAULT_MIN_HOP_RANK_INCREASE: u16 = 256;
+
 pub const DEFAULT_DIO_INTERVAL_MIN: u32 = 12;
 pub const DEFAULT_DIO_REDUNDANCY_CONSTANT: usize = 10;
 /// This is 20 in the standard, but in Contiki they use:
