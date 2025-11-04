ff2bdbe0b6203361a63c20cd1b9535a0a4f729ad
Aaron Davies <adavies@anduril.com>
2025-11-04 07:24:46 -0800

    update unit test with non-zero values to parse

--- src/wire/ipv4.rs
+++ src/wire/ipv4.rs
@@ -736,7 +736,7 @@ pub(crate) mod test {
     pub(crate) const MOCK_UNSPECIFIED: Address = Address::UNSPECIFIED;
 
     static PACKET_BYTES: [u8; 30] = [
-        0x45, 0x00, 0x00, 0x1e, 0x01, 0x02, 0x62, 0x03, 0x1a, 0x01, 0xd5, 0x6e, 0x11, 0x12, 0x13,
+        0x45, 0x21, 0x00, 0x1e, 0x01, 0x02, 0x62, 0x03, 0x1a, 0x01, 0xd5, 0x4d, 0x11, 0x12, 0x13,
         0x14, 0x21, 0x22, 0x23, 0x24, 0xaa, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff,
     ];
 
@@ -747,8 +747,8 @@ pub(crate) mod test {
         let packet = Packet::new_unchecked(&PACKET_BYTES[..]);
         assert_eq!(packet.version(), 4);
         assert_eq!(packet.header_len(), 20);
-        assert_eq!(packet.dscp(), 0);
-        assert_eq!(packet.ecn(), 0);
+        assert_eq!(packet.dscp(), 8);
+        assert_eq!(packet.ecn(), 1);
         assert_eq!(packet.total_len(), 30);
         assert_eq!(packet.ident(), 0x102);
         assert!(packet.more_frags());
@@ -756,7 +756,7 @@ pub(crate) mod test {
         assert_eq!(packet.frag_offset(), 0x203 * 8);
         assert_eq!(packet.hop_limit(), 0x1a);
         assert_eq!(packet.next_header(), Protocol::Icmp);
-        assert_eq!(packet.checksum(), 0xd56e);
+        assert_eq!(packet.checksum(), 0xd54d);
         assert_eq!(packet.src_addr(), Address::new(0x11, 0x12, 0x13, 0x14));
         assert_eq!(packet.dst_addr(), Address::new(0x21, 0x22, 0x23, 0x24));
         assert!(packet.verify_checksum());
@@ -770,8 +770,8 @@ pub(crate) mod test {
         packet.set_version(4);
         packet.set_header_len(20);
         packet.clear_flags();
-        packet.set_dscp(0);
-        packet.set_ecn(0);
+        packet.set_dscp(8);
+        packet.set_ecn(1);
         packet.set_total_len(30);
         packet.set_ident(0x102);
         packet.set_more_frags(true);
