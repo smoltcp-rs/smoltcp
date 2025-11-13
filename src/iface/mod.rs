59dee97fd5b50e4b9fae565f9ff2f7f7171ec26f
Jeffrey Moon <jmoon@anduril.com>
2025-11-13 11:24:41 -0500

    Expose neighbor module

--- src/iface/mod.rs
+++ src/iface/mod.rs
@@ -7,7 +7,7 @@ provides lookup and caching of hardware addresses, and handles management packet
 mod fragmentation;
 mod interface;
 #[cfg(any(feature = "medium-ethernet", feature = "medium-ieee802154"))]
-mod neighbor;
+pub mod neighbor;
 mod route;
 #[cfg(feature = "proto-rpl")]
 mod rpl;
