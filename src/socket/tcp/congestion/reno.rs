7021ab6dd9b1604297758dc32855f1e57fac0a59
Joe Dye <jpzh.dye@gmail.com>
2026-05-06 15:35:11 +0100

    change Reno to use new `Controller` methods and overhaul Reno to be RFC compliant: - enter slow start (and exit fast recovery) on RTO - prevent multiple `on_loss()` calls triggring window reductions - deflate `cwnd` when leaving fast recovery - cap slow start `cwnd` increment to 1MSS per ACK - use new `in_flight` to calcualte `ssthresh`

--- src/socket/tcp/congestion/reno.rs
+++ src/socket/tcp/congestion/reno.rs
@@ -9,6 +9,7 @@ pub struct Reno {
     min_cwnd: usize,
     ssthresh: usize,
     rwnd: usize,
+    recovery_start: Option<Instant>,
 }
 
 impl Reno {
@@ -18,6 +19,7 @@ impl Reno {
             min_cwnd: 1024 * 2,
             ssthresh: usize::MAX,
             rwnd: 64 * 1024,
+            recovery_start: None,
         }
     }
 }
@@ -27,28 +29,58 @@ impl Controller for Reno {
         self.cwnd
     }
 
-    fn on_ack(&mut self, _now: Instant, len: usize, _rtt: &RttEstimator) {
-        let len = if self.cwnd < self.ssthresh {
-            // Slow start.
-            len
+    fn on_ack(&mut self, _now: Instant, len: usize, _in_flight: usize, _rtt: &RttEstimator) {
+        // First new-data-ack exits fast recovery and deflates `cwnd`
+        if self.recovery_start.is_some() {
+            self.recovery_start = None;
+            self.cwnd = self.ssthresh;
+            return;
+        }
+
+        let inc = if self.cwnd < self.ssthresh {
+            // Slow start: increase `cwnd` by 1 MSS per ACK.
+            len.min(self.min_cwnd)
         } else {
-            self.ssthresh = self.cwnd;
-            self.min_cwnd
+            // Congestion avoidance: increase by ~1 MSS per RTT.
+            (self.min_cwnd * self.min_cwnd / self.cwnd).max(1)
         };
 
         self.cwnd = self
             .cwnd
-            .saturating_add(len)
+            .saturating_add(inc)
             .min(self.rwnd)
             .max(self.min_cwnd);
     }
 
-    fn on_duplicate_ack(&mut self, _now: Instant) {
-        self.ssthresh = (self.cwnd >> 1).max(self.min_cwnd);
+    fn on_dup_ack(&mut self, _now: Instant, len: usize, _in_flight: usize) {
+        if self.recovery_start.is_some() {
+            self.cwnd = self
+                .cwnd
+                .saturating_add(len)
+                .min(self.rwnd)
+                .max(self.min_cwnd);
+        }
     }
 
-    fn on_retransmit(&mut self, _now: Instant) {
-        self.cwnd = (self.cwnd >> 1).max(self.min_cwnd);
+    fn on_loss(&mut self, now: Instant, in_flight: usize) {
+        // Only cut window size on first entrance to fast recovery.
+        if self.recovery_start.is_none() {
+            self.ssthresh = (in_flight >> 1).max(2 * self.min_cwnd);
+            self.cwnd = self
+                .ssthresh
+                .saturating_add(3 * self.min_cwnd)
+                .min(self.rwnd);
+
+            self.recovery_start = Some(now);
+        }
+    }
+
+    fn on_rto(&mut self, _now: Instant, in_flight: usize) {
+        self.ssthresh = (in_flight >> 1).max(2 * self.min_cwnd);
+        self.cwnd = self.min_cwnd;
+
+        // Major loss has occurred, ensure we move from fast recovery (if in it) to slow start.
+        self.recovery_start = None
     }
 
     fn set_mss(&mut self, mss: usize) {
@@ -81,7 +113,7 @@ mod test {
                 // Set remote window.
                 reno.set_remote_window(remote_window);
 
-                reno.on_ack(now, 4096, &RttEstimator::default());
+                reno.on_ack(now, 4096, reno.window(), &RttEstimator::default());
 
                 let mut n = i;
                 for _ in 0..j {
@@ -89,13 +121,13 @@ mod test {
                 }
 
                 if i & 1 == 0 {
-                    reno.on_retransmit(now);
+                    reno.on_rto(now, reno.window());
                 } else {
-                    reno.on_duplicate_ack(now);
+                    reno.on_loss(now, reno.window());
                 }
 
                 let elapsed = Instant::from_millis(1000);
-                reno.on_ack(elapsed, n, &RttEstimator::default());
+                reno.on_ack(elapsed, n, reno.window(), &RttEstimator::default());
 
                 let cwnd = reno.window();
                 println!("Reno: elapsed = {}, cwnd = {}", elapsed, cwnd);
@@ -115,7 +147,7 @@ mod test {
         reno.set_remote_window(remote_window);
 
         for _ in 0..100 {
-            reno.on_retransmit(now);
+            reno.on_rto(now, reno.window());
             assert!(reno.window() >= reno.min_cwnd);
         }
     }
